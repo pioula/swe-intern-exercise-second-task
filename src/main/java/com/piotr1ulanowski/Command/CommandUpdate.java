defb2d40e6a692957fdb01a7d4e1a6be389889e4
Piotr Ulanowski <pioula111@gmail.com>
2022-11-25 18:59:47 +0100

    Updated commands

--- src/main/java/com/piotr1ulanowski/Command/CommandUpdate.java
+++ src/main/java/com/piotr1ulanowski/Command/CommandUpdate.java
@@ -1,12 +1,10 @@
 package com.piotr1ulanowski.Command;
 
-import com.piotr1ulanowski.Server.BroadcastMessage;
 import com.piotr1ulanowski.User.User;
 import com.piotr1ulanowski.User.UserProperty;
 
 import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Optional;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class CommandUpdate implements CommandI {
     private final String user;
@@ -21,22 +19,13 @@ public class CommandUpdate implements CommandI {
 
     // Removing users from friend lists of each user.
     @Override
-    public Optional<String> execute(HashMap<String, User> users) {
-        if (!users.containsKey(user))
-            users.put(user, new User());
+    public void execute(ConcurrentHashMap<String, User> users) {
+        users.putIfAbsent(user, new User());
 
         User userObject = users.get(user);
-        HashMap<String, String> addedValues = new HashMap<>();
-        values.forEach((propertyName, value) -> {
-            if (!userObject.addProperty(propertyName, new UserProperty(value, timestamp)))
-                addedValues.put(propertyName, value);
-        });
 
-        users.put(user, userObject);
-        BroadcastMessage broadcastMessage
-                = new BroadcastMessage(userObject.getFriends(), user, addedValues, timestamp);
-
-        String result = broadcastMessage.toString();
-        return result.equals("") ? Optional.empty() : Optional.of(result);
+        values.forEach((propertyName, value) ->
+            userObject.addProperty(propertyName, new UserProperty(value, timestamp))
+        );
     }
 }
